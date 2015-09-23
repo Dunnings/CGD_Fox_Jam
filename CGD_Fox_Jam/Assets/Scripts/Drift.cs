f85c2ac88e876b1a2829ba2f045b554c52dd3482
Dunnings <mrdaviddunnings@gmail.com>
2015-09-23 12:34:33 +0100

    Working on game world generation

---
+++ CGD_Fox_Jam/Assets/Scripts/Drift.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drift : MonoBehaviour {
+
+    public float m_speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+        m_speed = Random.Range(m_speed - 0.1f, m_speed + 0.2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position += new Vector3(-m_speed * Time.deltaTime, 0f, 0f);
+	}
+}
---
+++ CGD_Fox_Jam/Assets/WorldGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldGenerator : MonoBehaviour {
+
+    public Sprite surfaceTile;
+    public float surfacePosition = 2.7f;
+    public int surfaceWidth = 100;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < surfaceWidth; i++)
+        {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
