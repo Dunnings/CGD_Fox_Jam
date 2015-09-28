f2aed75350c657e4a45263347edc1f14294e0c1a
Dunnings <mrdaviddunnings@gmail.com>
2015-09-28 13:26:19 +0100

    World Generation now spawns borders Updated Main Scene

--- CGD_Fox_Jam/Assets/Scripts/Enemy.cs
+++ CGD_Fox_Jam/Assets/Scripts/Enemy.cs
@@ -211,7 +211,7 @@ public class Enemy : MonoBehaviour
         pos.y = WorldGenerator.Instance.m_surfacePos;
         deathParticles.transform.position = pos;
         Destroy(deathParticles, 6f);
-        SFXManager.Instance.PlayDeathSound();
+        FindObjectOfType<SFXManager>().PlayDeathSound();
         Destroy(gameObject);        
     }
 }
--- CGD_Fox_Jam/Assets/Scripts/SFXManager.cs
+++ CGD_Fox_Jam/Assets/Scripts/SFXManager.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class SFXManager : MonoBehaviour {
-    public static SFXManager Instance;
+
+    public List<AudioClip> deathSounds = new List<AudioClip>();
 
     public AudioSource m_source;
 
 	// Use this for initialization
 	void Start () {
-        Instance = this;
 	}
 	
 	// Update is called once per frame
@@ -18,7 +18,9 @@ public class SFXManager : MonoBehaviour {
 
     public void PlayDeathSound()
     {
-        m_source.pitch = Random.Range(0.8f, 1.2f);  
+        m_source.pitch = Random.Range(0.8f, 1.2f);
+        int randNo = Random.Range(0, deathSounds.Count - 1);
+        m_source.clip = deathSounds[randNo];
         m_source.Play();
     }
 }
