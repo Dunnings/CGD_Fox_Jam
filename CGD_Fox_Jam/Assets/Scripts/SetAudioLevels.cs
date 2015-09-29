6c4e4c9fe49b7ad23e96af9f636661fad6199623
bourneofunction <oliver2.bourne@live.uwe.ac.uk>
2015-09-29 17:16:54 +0100

    Options Panel
    Managed to redo what I did earlier before me and Github had a falling
    out... Options panel now changes audio levels for both music and soundfx

---
+++ CGD_Fox_Jam/Assets/Scripts/SetAudioLevels.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetAudioLevels : MonoBehaviour {
+
+    public AudioMixer mainMixer;
+    
+    public void SetMusicLevel(float musicLevel)
+    {
+        mainMixer.SetFloat("musicVol", musicLevel);
+    }
+
+    public void SetSfxLevel(float sfxLevel)
+    {
+        mainMixer.SetFloat("sfxVol", sfxLevel);
+    }
+}
